95c50f6f3fc406febfe115e3765a2fc539f2013c
James <asignoflife@gmail.com>
2020-02-24 23:35:26 -0800

    Updates

---
+++ precache-manifest.0187314e0c4bced08e45bb93807153e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071e1179d87df9a6c4ca351ce1ee72a3",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a953c9070763ed4e6508",
+    "url": "/monster-rolodex/static/css/main.430c012a.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a953c9070763ed4e6508",
+    "url": "/monster-rolodex/static/js/main.4666f748.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
